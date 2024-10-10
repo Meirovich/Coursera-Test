c5a1e2dd31b8ce5b74bf833f4a13da6165ac4f6a
Your Name <Eitan@Meirovich.com>
2024-10-10 17:00:51 -0300

    MyFirstApp

--- Lecture 4/app.js
+++ Lecture 4/app.js
@@ -1,9 +1,12 @@
-(Function(){
+(function(){
 'use strict';
 
 angular.module('myFirstApp',[])
-.controller('MyFirstController', function(){
-
+.controller('MyFirstController', function($scope){
+$scope.name="Eitan";
+$scope.sayhello= function(){
+  return "Hello Caro!"
+};
 });
 
 })();
